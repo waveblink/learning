640c8cd96de16b2abc9ac80d9abe4605db3117ef
waveblink <maxwellhahn@gmail.com>
2025-06-01 12:14:21 -0500

    Last part of q4

--- helloWorld.py
+++ helloWorld.py
@@ -8,15 +8,12 @@ print(f"Welcome, {player_name}, to the goblin dungeon!")
 while counter_right < 3 and counter_left < 3:
     walking_choice = input("Do you want to go left or right? ")
     if walking_choice == "left":
+        counter_left += 1
         print("You find a treasure chest!")
     elif walking_choice == "right":
+        counter_right += 1
         print("You find a monster!")
 
-
-if walking_choice == "right":
-    counter_right += 1
-elif walking_choice == "left":
-    counter_left += 1
 
 if counter_right == 3:
     print("You have died!")
