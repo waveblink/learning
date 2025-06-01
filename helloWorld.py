5643002c842255079d29a20714c27f0d6550e660
waveblink <maxwellhahn@gmail.com>
2025-06-01 12:05:23 -0500

    Fixed ==, moved game logic inside the loop

--- helloWorld.py
+++ helloWorld.py
@@ -1,21 +1,23 @@
-counter_right == 0
-counter_left == 0
+counter_right = 0
+counter_left = 0
 
 player_name = input("What is your name, Adventurer? ")
 print(f"Welcome, {player_name}, to the goblin dungeon!")
+
+
+walking_choice = input("Do you want to go left or right? ")
+
+
 while counter_right or counter_left < 3:
-    walking_choice = input("Do you want to go left or right? ")
-
-
-if walking_choice == "left":
-    print("You find a treasure chest!")
-elif walking_choice == "right":
-    print("You find a monster!")
+    if walking_choice == "left":
+        print("You find a treasure chest!")
+    elif walking_choice == "right":
+        print("You find a monster!")
 
 
 if walking_choice == "right":
     counter_right += 1
-else:
+elif walking_choice == "left":
     counter_left += 1
 
 if counter_right == 3:
